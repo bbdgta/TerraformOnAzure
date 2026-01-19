8072f265f1bb16b775a5b3b0dd53eed31a01c178
Sourabh Sharma <108170306+bbdgta@users.noreply.github.com>
2026-01-19 16:54:03 +0000

    dynamic addition

--- Day05/main.tf
+++ Day05/main.tf
@@ -3,15 +3,27 @@ resource "azurerm_resource_group" "example" {
   location = var.allowed_locations[0]
 }
 
-resource "azurerm_storage_account" "example" {
-  for_each = var.storage_account_name
-  name                     = each.value
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
+  dynamic "security_rule" {
+    for_each = local.nsg_rules
+    content {
+      name                       = security_rule.key
+      priority                   = security_rule.value.priority
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = security_rule.value.destination_port_range
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  }
+  
   tags = {
-    environment = "staging"
+    environment = "Production"
   }
 }
